a2d301a3d6758053c0131aa2ee8909ebc4844ce2
Huijiny <jiin20803@gmail.com>
2021-05-20 13:20:26 +0900

    Add signup design

---
+++ src/views/SignUp.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="bgimg d-flex justify-content-center align-items-center">
+    <div class="auth-size">
+      <div class="d-flex justify-content-left">
+        <h2>Signup</h2>
+      </div>
+      <div class="input-group">
+        <input v-model="username" type="text" class="form-control block" placeholder="이름(2자 이상)">
+      </div>
+      <div class="input-group">
+        <input v-model="password" type="password" class="form-control" placeholder="영문, 숫자, 특문 증 2개 조합 10자 이상">
+      </div>
+      <div class="input-group">
+        <input v-model="passwordConfirmation" type="password" class="form-control" placeholder="위 비밀번호를 그대로 쳐주세요.">
+      </div>
+      <button @click="signup" class="btn-grad btn-size btn-text m-3">회원가입</button>
+      <p >저희 서비스는 무료입니다. 회원가입이 간단하죠.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  data: function () {
+    return {
+      username: null,
+      password: null,
+      passwordConfirmation: null,
+    }
+  },
+  methods: {
+    signup: function () {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.btn-grad {background-image: linear-gradient(to left, #FC466B 0%, #4A00E0  50%, #FC466B  100%)}
+.btn-grad {
+  margin: 10px;
+  padding: 10px 45px;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;            
+}
+.btn-text {
+  text-transform: uppercase;
+  text-align: center;
+  font-family: 'Noto Sans KR', sans-serif;
+}
+.btn-size {
+  width: 70%;
+  border-radius: 30px;
+  border: none;
+}
+.btn-grad:hover {
+  background-position: right center; /* change the direction of the change here */
+  color: #fff;
+  text-decoration: none;
+}
+.bgimg {
+  /* Set rules to fill background */
+  min-height: 100%;
+  min-width: 1024px;
+    
+  /* Set up proportionate scaling */
+  width: 100%;
+  height: auto;
+    
+  /* Set up positioning */
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  background-image: url('~@/assets/Joker.png');
+  background-size: 100% 100%;
+
+}
+.auth-size {
+  width: 20%;
+}
+p {
+  color: #DBDBDB;
+}
+
+h2 {
+  color: #FFFFFF;
+}
+</style>
