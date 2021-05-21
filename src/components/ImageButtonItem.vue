1365a57004c5b4e2f22832dfbcc1ecdfc8cc6948
Huijiny <jiin20803@gmail.com>
2021-05-21 12:09:59 +0900

    Add hover effect

--- src/components/ImageButtonItem.vue
+++ src/components/ImageButtonItem.vue
@@ -1,9 +1,15 @@
 <template>
-<div class="col">
-  <div class="center-cropped">
-    <img :src="item.actor_poster_path"/>
+  <div class="item-wrapper col">
+    <div class="center-cropped">
+      <img :src="item.actor_poster_path"/>
+    </div>
+    <div class="overlay">
+      <div class="text">
+        <h5>{{ item.actorname }}</h5>
+        <button class="btn btn-link"><i class="fas fa-heart like-button"></i></button>
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -18,23 +24,54 @@ export default {
 </script>
 
 <style scoped>
-  /* .item-wrapper {
-      position:relative;
-      display:inline-block;
+
+.item-wrapper {
+  position: relative;
+  margin: 1% 1%;
+}
+  div {
+    display: block
   }
-  .item-wrapper:hover:after {
-      content:"\asdfasdfasdf";
-      width:100%;
-      height:100%;
-      background:rgba(0, 0, 0, 0.5);
-      position:absolute;
-      top:0;
-      left:0;
-  } */
+  .overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: .3s ease;
+    background-color: rgba(18, 17, 24, 0.5);
+}
+.item-wrapper:hover .overlay {
+  opacity: 1;
+}
   img {
-    width: 300px;
-    height: 400px;
+    display: block;
+    width: 100%;
+    height: auto;
     object-fit: cover; 
     object-position: center;
   }
+  /* * ::before, ::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+  } */
+  .text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+.like-button {
+  color: #FF89B6;
+  font-size: 30px;
+}
 </style>
--- src/views/SelectActors.vue
+++ src/views/SelectActors.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="container">
-  <div class="navi mt-5 mb-3">
+<div class="container  navi-margin">
+  <div class="navi css-q46ro5 pt-4 pb-5">
     <div class="guide-text">
       <h5><span>{{ user.username }}</span>님, 당신의 취향을 알고싶어요! </h5>
       <h5>좋아하는 배우를 선택해주세요.</h5>
@@ -112,5 +112,21 @@ export default {
   }
   .navi {
     position: sticky;
+  }
+  .css-q46ro5 {
+    /* background: linear-gradient( to top, rgba(18,18,18,0), rgba(18,18,18,0.6) 10%, rgba(18,18,18,0.8) 16%, rgba(18,18,18,1) 29% );
+    height: 237px; */
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 400;
+    background: linear-gradient( to top, rgba(18,18,18,0), rgba(18,18,18,1) 20% );
+    text-align: center;
+    width: 100%;
+    height: 100px;
+  }
+  .navi-margin {
+    margin-top: 100px;
   }
 </style>
