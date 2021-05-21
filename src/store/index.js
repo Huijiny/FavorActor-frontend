e43f14b6f71e6f051f7cd2475e2996e01f692643
Huijiny <jiin20803@gmail.com>
2021-05-21 19:40:54 +0900

    Set usertoken in vuex and get user information in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import router from "../router"; 
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    usertoken: '',
+    user: {
+      username: '',
+      favoriteActors: [],
+      favoriteMovies: [],
+    },
   },
   mutations: {
+    SET_USER_TOKEN: function (state, token) {
+      state.usertoken = token
+    },
+    SET_USER_DATA: function (state, userdata) {
+      state.user = userdata
+    }
   },
   actions: {
+    setUserToken: function ({ commit }, token) {
+      commit('SET_USER_TOKEN', token)
+
+      axios({
+        method: 'post',
+        url: 'http://127.0.0.1:8000/accounts/login/',
+        headers: { Authorization: `JWT ${token}` },
+      })
+        .then(res => {
+          commit('SET_USER_DATA', res.data)
+          return res.data
+        })
+        .then(res => {
+          if (res.favor_actors_id.length == 0) {
+            router.push({ name: 'SelectActors' })
+          } else {
+            router.push({ name: 'Main'})
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })      
+    }
   },
   modules: {
   }
