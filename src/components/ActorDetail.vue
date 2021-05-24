4e188225e186cc439db00ee26735d912c7a092db
Huijiny <jiin20803@gmail.com>
2021-05-24 16:42:00 +0900

    Add Progressbar dependency and add some design

--- src/components/ActorDetail.vue
+++ src/components/ActorDetail.vue
@@ -1,22 +1,28 @@
 <template>
-  <div> 
-    <div class="center-cropped">
+  <div class="d-flex"> 
+    <div class="center-cropped actor-img">
       <img :src="getImage"/>
     </div>
     <div>
       <p>출생: {{ this.actor.birth }}</p>
       <p>배우가 출연한 영화의 평균 평점: {{ this.voteAverage }}</p>
+      <KProgress :percent="60" :color="['#f5af19', '#f12711', '#9254de', '#40a9ff', '#5cdbd3']" :border="false"></KProgress>
     </div>
   </div>
 </template>
 
 <script>
+import KProgress from 'k-progress';
+
 export default {
   name: 'ActorDetail',
   props: {
     actor: {
       type: Object
     },
+  },
+  components: {
+    KProgress,
   },
   computed: {
     getImage: function () {
@@ -30,11 +36,23 @@ export default {
       }, 0)
       console.log(sumOfVote)
       return (sumOfVote / length).toFixed(1)
+    },
+    votePercentage: function () {
+      return this.voteAverage * 100
     }
+  },
+  mounted: function () {
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.actor-img {
+  width: 100px;
+}
+#progress-bar {
+  margin: 20px;
+  width: 400px;
+  height: 8px;
+}
 </style>
